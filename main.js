36686e475fd88de745407dafdc86a7d83018d1db
Gennadiy Saranchuk <alooha.broo@gmail.com>
2024-10-31 12:40:31 +0500

    feat: add script date

--- main.js
+++ main.js
@@ -6,3 +6,25 @@ document.addEventListener("DOMContentLoaded", function () {
     navigation.classList.toggle("header__navigation--active");
   });
 });
+
+const today = new Date();
+
+const yesterday = new Date(today);
+yesterday.setDate(today.getDate() - 1);
+document.getElementById(
+  "author__date"
+).textContent = `Published : ${yesterday.toLocaleDateString()}`;
+
+const threeDaysAgo = new Date(today);
+threeDaysAgo.setDate(today.getDate() - 3);
+const elements = document.querySelectorAll(".three_days_ago");
+
+elements.forEach((element) => {
+  element.textContent = `${threeDaysAgo.toLocaleDateString()}`;
+});
+
+const tomorrow = new Date(today);
+tomorrow.setDate(today.getDate() + 1);
+document.getElementById(
+  "tomorrow"
+).textContent = `${tomorrow.toLocaleDateString()}`;
